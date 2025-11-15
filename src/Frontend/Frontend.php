cea7bf3795e5723eb2f25c59949f69e88e308770
unknown <mahbub.progressivebyte@gmail.com>
2025-11-15 10:51:11 +0600

    misc

--- src/Frontend/Frontend.php
+++ src/Frontend/Frontend.php
@@ -6,34 +6,14 @@ use DNS\Helper\Messages;
 class Frontend {
 
     public function __construct() {
-        // add_action('wp_footer', [ $this, 'show_notification'] );
         add_action('wp_head', [ $this, 'head'] );
     }
 
-    public function head(){
-        $listings = get_posts([
-                'post_type'      => 'at_biz_dir',
-                'posts_per_page' => -1,
-                'orderby'        => 'title',
-                'order'          => 'ASC',
-            ]);
+    public function head(){       
 
         // if ( ! empty( $listings )){
         //     Messages::pri( $listings );
         // }
 
-    }
-
-    public function show_notification() {
-        echo '<div id="dns-notification" style="position: fixed; bottom: 20px; right: 20px; background: #0073aa; color: #fff; padding: 15px; border-radius: 5px; display:none;">
-                This is a directory notification!
-              </div>';
-        ?>
-        <script>
-        jQuery(document).ready(function($){
-            $('#dns-notification').fadeIn().delay(3000).fadeOut();
-        });
-        </script>
-        <?php
     }
 }
