dcbeb2c73c629a8fe6c59c14683686c6bdaf1279
unknown <mahbub.progressivebyte@gmail.com>
2025-11-16 15:13:13 +0600

    in settings page now user are shown

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -1,6 +1,8 @@
 <?php
 namespace DNS\Admin;
 
+use DNS\Helper\Messages;
+
 class Admin {
 
     public function __construct() {
@@ -41,8 +43,8 @@ class Admin {
     }
 
 
-    /**
-     * Admin page content
+       /**
+     * Admin Page Content
      */
     public function admin_page() {
         // Get all pages
@@ -53,8 +55,12 @@ class Admin {
         $job_page         = get_option('dns_subscription_page_job');
         $product_enabled  = get_option('dns_subscribe_product');
         $product_page     = get_option('dns_subscription_page_product');
-        $selected_page    = get_option('dns_subscription_page_id'); // for generic subscription page if needed
+        $selected_page    = get_option('dns_subscription_page_id');
+
+        // Notification Logs
+        $logs = get_option('dns_notification_logs', []);
         ?>
+        
         <div class="dns-admin-wrap">
             <h1>Directory Notifications Admin</h1>
             <p>Manage all directory notifications here.</p>
@@ -63,9 +69,10 @@ class Admin {
             <h2 class="nav-tab-wrapper">
                 <a href="#tab-settings" class="nav-tab nav-tab-active">Settings</a>
                 <a href="#tab-log" class="nav-tab">Log</a>
+                <a href="#tab-subscribed" class="nav-tab">Subscribed Users</a>
             </h2>
 
-            <!-- Tabs Content -->
+            <!-- SETTINGS TAB -->
             <div id="tab-settings" class="tab-content" style="display:block;">
                 <form method="post" action="options.php">
                     <?php
@@ -76,21 +83,21 @@ class Admin {
                     <h2>Subscribe Button Settings</h2>
 
                     <table class="form-table">
-                        <!-- Job Listings -->
-                        <tr valign="top">
-                            <th scope="row">Add "Subscribe to Notifications" button on Job Listings page</th>
+
+                        <!-- JOB LISTINGS -->
+                        <tr>
+                            <th scope="row">Subscribe Button on Job Listings</th>
                             <td>
                                 <label class="dns-toggle-wrapper">
                                     <span class="dns-toggle">
-                                        <input type="checkbox" id="dns_subscribe_job" name="dns_subscribe_job" value="1" <?php checked(1, $job_enabled, true); ?> />
+                                        <input type="checkbox" id="dns_subscribe_job" name="dns_subscribe_job" value="1" <?php checked($job_enabled, 1); ?> />
                                         <span class="dns-toggle-slider"></span>
                                     </span>
                                     <span>Enable subscribe button on Job Listing pages</span>
                                 </label>
 
-                                <!-- Conditional Page Select -->
                                 <div id="dns_job_page_select" style="margin-top:10px; <?php echo $job_enabled ? '' : 'display:none;'; ?>">
-                                    <label>Select subscription page for Job Listings:</label>
+                                    <label>Select Subscription Page:</label>
                                     <select name="dns_subscription_page_job">
                                         <option value="">-- Select Page --</option>
                                         <?php foreach ($pages as $page) : ?>
@@ -103,21 +110,20 @@ class Admin {
                             </td>
                         </tr>
 
-                        <!-- Product Listings -->
-                        <tr valign="top">
-                            <th scope="row">Add "Subscribe to Notifications" button on Product Listings page</th>
+                        <!-- PRODUCT LISTINGS -->
+                        <tr>
+                            <th scope="row">Subscribe Button on Product Listings</th>
                             <td>
                                 <label class="dns-toggle-wrapper">
                                     <span class="dns-toggle">
-                                        <input type="checkbox" id="dns_subscribe_product" name="dns_subscribe_product" value="1" <?php checked(1, $product_enabled, true); ?> />
+                                        <input type="checkbox" id="dns_subscribe_product" name="dns_subscribe_product" value="1" <?php checked($product_enabled, 1); ?> />
                                         <span class="dns-toggle-slider"></span>
                                     </span>
                                     <span>Enable subscribe button on Product Listing pages</span>
                                 </label>
 
-                                <!-- Conditional Page Select -->
                                 <div id="dns_product_page_select" style="margin-top:10px; <?php echo $product_enabled ? '' : 'display:none;'; ?>">
-                                    <label>Select subscription page for Product Listings:</label>
+                                    <label>Select Subscription Page:</label>
                                     <select name="dns_subscription_page_product">
                                         <option value="">-- Select Page --</option>
                                         <?php foreach ($pages as $page) : ?>
@@ -130,9 +136,9 @@ class Admin {
                             </td>
                         </tr>
 
-                        <!-- General Subscription Page -->
-                        <tr valign="top">
-                            <th scope="row">Subscription Page</th>
+                        <!-- GENERAL SUB PAGE -->
+                        <tr>
+                            <th scope="row">Default Subscription Page</th>
                             <td>
                                 <select name="dns_subscription_page_id">
                                     <option value="">-- Select Page --</option>
@@ -142,26 +148,130 @@ class Admin {
                                         </option>
                                     <?php endforeach; ?>
                                 </select>
-                                <p class="description">Select the page where users will be redirected when clicking the "Subscribe to Notifications" button.</p>
+                                <p class="description">Used if no specific page is assigned.</p>
                             </td>
                         </tr>
+
                     </table>
 
                     <?php submit_button(); ?>
                 </form>
             </div>
 
-            <!-- Log Tab -->
+            <!-- LOG TAB -->
             <div id="tab-log" class="tab-content" style="display:none;">
                 <h2>Notification Log</h2>
-                <p>Here you can view the notification logs in future.</p>
-                <div id="dns-log-output">
-                    <!-- Log content can be added dynamically -->
-                </div>
+
+                <?php if (empty($logs)) : ?>
+                    <p>No logs available.</p>
+                <?php else : ?>
+                    <table class="widefat striped">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Message</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($logs as $log) : ?>
+                                <tr>
+                                    <td><?php echo esc_html($log['time']); ?></td>
+                                    <td><?php echo esc_html($log['msg']); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                <?php endif; ?>
             </div>
+
+            <!-- SUBSCRIBED USERS TAB -->
+            <div id="tab-subscribed" class="tab-content" style="display:none;">
+                <h2>Subscribed Users</h2>
+
+                <?php
+                $users = get_users(['meta_key' => 'dns_notify_prefs']);
+
+                if (empty($users)) :
+                    echo "<p>No subscribed users found.</p>";
+                else :
+                ?>
+
+                <table class="widefat fixed striped">
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Listing Types</th>
+                            <th>Locations</th>
+                            <th>Listings</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($users as $user) : 
+                        $prefs = get_user_meta($user->ID, 'dns_notify_prefs', true);
+                        $prefs = wp_parse_args($prefs, [
+                            'listing_types' => [],
+                            'listing_locations' => [],
+                            'listing_posts' => [],
+                        ]);
+                    ?>
+                        <tr>
+                            <td>
+                                <strong><?php echo esc_html($user->display_name); ?></strong><br>
+                                <small><?php echo esc_html($user->user_email); ?></small>
+                            </td>
+
+                            <td>
+                                <?php
+                                if (!empty($prefs['listing_types'])) {
+                                    foreach ($prefs['listing_types'] as $tid) {
+                                        $term = get_term($tid);
+                                        if ($term) echo esc_html($term->name) . "<br>";
+                                    }
+                                } else {
+                                    echo "<em>None</em>";
+                                }
+                                ?>
+                            </td>
+
+                            <td>
+                                <?php
+                                if (!empty($prefs['listing_locations'])) {
+                                    foreach ($prefs['listing_locations'] as $lid) {
+                                        $term = get_term($lid);
+                                        if ($term) echo esc_html($term->name) . "<br>";
+                                    }
+                                } else {
+                                    echo "<em>None</em>";
+                                }
+                                ?>
+                            </td>
+
+                            <td>
+                                <?php
+                                if (!empty($prefs['listing_posts'])) {
+                                    foreach ($prefs['listing_posts'] as $pid) {
+                                        $post = get_post($pid);
+                                        if ($post) echo esc_html($post->post_title) . "<br>";
+                                    }
+                                } else {
+                                    echo "<em>None</em>";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+                <?php endif; ?>
+
+            </div>
+
         </div>
+
         <?php
     }
+
 
 
 
